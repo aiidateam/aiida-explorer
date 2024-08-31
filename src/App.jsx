c57a355336c789c530b0073dfac19c88c648659b
Kristjan Eimre <kristjaneimre@gmail.com>
2024-08-31 18:40:38 +0300

    BrowserRouter: handle basename correctly

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ const App = () => {
 
   return (
     <div className="p-1">
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
         <Routes>
           <Route
             path="/*"
