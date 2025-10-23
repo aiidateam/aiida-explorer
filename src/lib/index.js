f12e5fc02c3a1e4ad7020366ee413fef07ea9c42
macaul_b <budmacaulay97@outlook.com>
2025-10-23 14:22:11 +0200

    added vis to public api

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,12 @@
 // Public API for the component library
 export { default as AiidaExplorer } from "../AiidaExplorer/index.jsx";
+
+// raw vis
 export { default as RawDataVisualiser } from "../AiidaExplorer/VisualiserPane/RawDataVisualiser.jsx";
+
+// rich vis
 export { default as StructureDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/StructureDataVisualiser.jsx";
+export { default as KpointsDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/StructureDataVisualiser.jsx";
+export { default as BandsDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/BandsDataVisualiser.jsx";
+export { default as CalcJobVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/CalcJobVisualiser.jsx";
+export { default as UpfDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/UpfDataVisualiser.jsx";
