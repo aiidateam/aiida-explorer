e1211644da32a41fb57f50412a4459d2d52bd70a
macaul_b <budmacaulay97@outlook.com>
2025-10-31 16:49:43 +0100

    fix exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,8 +5,8 @@ export { default as AiidaExplorer } from "../AiidaExplorer/index.jsx";
 export { default as RawDataVisualiser } from "../AiidaExplorer/VisualiserPane/RawDataVisualiser.jsx";
 
 // rich vis
-export { default as StructureDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/StructureDataVisualiser.jsx";
-export { default as KpointsDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/StructureDataVisualiser.jsx";
+export { default as StructureDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/StructureDataVisualiser";
+export { default as KpointsDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/KpointsDataVisualiser";
 export { default as BandsDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/BandsDataVisualiser.jsx";
 export { default as CalcJobVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/CalcJobVisualiser.jsx";
 export { default as UpfDataVisualiser } from "../AiidaExplorer/VisualiserPane/Rich/UpfDataVisualiser.jsx";
